8707c555b77cab525800c881b83a2bea699f5675
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-25 14:41:01 -0500

    add #map and size constraints to Map

--- website/src/lib/components/Map.svelte
+++ website/src/lib/components/Map.svelte
@@ -1,3 +1,14 @@
 <script lang="ts">
     import L from "leaflet"
 </script>
+
+<div id="map">
+</div>
+
+<style>
+    #map {
+        height: min(100cqw, 100cqh);
+        width: min(100cqw, 100cqh);
+        aspect-ratio: 1 / 1;
+    }
+</style>
