a33a98f6da3bfd488efdcb27e3b76db02f3720cd
Alí Sinisterra <haidarconh@gmail.com>
2025-10-25 18:48:25 -0500

    create, start work on semantics: Navigation component

---
+++ website/src/lib/sections/Navigation.svelte
@@ -0,0 +1,10 @@
+
+<div>
+    <div>
+        <span class="number">1</span>
+        <div class="info">
+            <p class="site-name">Ali's Website</p>
+            <p class="site-address">12 Raspberry Way, HUK</p>
+        </div>
+    </div>
+</div>
