a14dc7561194fd1a3d2c8c9ead0ded67ad86b76e
Alí Sinisterra <haidarconh@gmail.com>
2025-10-25 18:22:21 -0500

    set flex, width, and item alignment of Header

--- website/src/lib/sections/Header.svelte
+++ website/src/lib/sections/Header.svelte
@@ -3,6 +3,13 @@
 </header>
 
 <style>
+    header {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
     h1 {
         font-size: var(--c-font-size-AA);
         font-family: var(--c-font-family-display);
