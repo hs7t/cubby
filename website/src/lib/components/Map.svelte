2f7ee1a4f2f572374e90926475e23bf5650770d5
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-25 15:03:31 -0500

    update Map to work properly with Svelte

--- website/src/lib/components/Map.svelte
+++ website/src/lib/components/Map.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
+    import { onMount, onDestroy } from "svelte";
     import L, { type LatLngBoundsExpression } from "leaflet"
     import LargeMapImg from '../../assets/Large Map.svg'
 
     let mapElement
+    let map: L.Map
 
-    let map = L.map('map', {
-        crs: L.CRS.Simple
-    });
-    let bounds: LatLngBoundsExpression = [[0, 0], [1000, 1000]]
-    let image = L.imageOverlay(LargeMapImg, bounds).addTo(map)
+    onMount(async () => {
+        map = L.map('map', {
+            crs: L.CRS.Simple
+        });
+        let bounds: LatLngBoundsExpression = [[0, 0], [1000, 1000]]
+        let image = L.imageOverlay(LargeMapImg, bounds).addTo(map)
 
-    map.fitBounds(bounds);
+        map.fitBounds(bounds);
+    })
+
+    onDestroy(async () => {
+        if (map) {
+            console.log("Unloading map!")
+            map.remove()
+        }
+    })
 </script>
 
 <div id="map" bind:this={mapElement}>
