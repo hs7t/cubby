3966060969aabb46297c732e7cef57d36cb59684
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 18:56:00 -0500

    handle Navigation .navigator overflow

--- website/src/lib/sections/Navigation.svelte
+++ website/src/lib/sections/Navigation.svelte
@@ -29,6 +29,8 @@
         display: flex;
         flex-direction: column;
         justify-content: flex-end;
+        max-height: 100%;
+        overflow-y: scroll;
         flex-grow: 1;
         gap: 0.2em;
     }
