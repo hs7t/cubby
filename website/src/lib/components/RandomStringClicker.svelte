a3f65758e16b295b8e0f9b1f44ab94553fa9c37a
Alí Sinisterra <haidarconh@gmail.com>
2025-10-26 10:48:19 -0500

    create RandomStringClicker

---
+++ website/src/lib/components/RandomStringClicker.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let { strings = [] as Array<string> } = $props()
+    
+    let currentString = $state("")
+    const randomString = () => {
+        const randomIndex = Math.floor(Math.random() * strings.length)
+        return strings[randomIndex]
+    }
+    const updateString = () => {
+        currentString = randomString()
+    }
+    updateString()
+</script>
+
+<button onclick={updateString}>
+    {currentString}
+</button>
