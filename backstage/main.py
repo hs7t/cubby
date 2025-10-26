f43490cd202bd3e8ee07ceef84d44e3f565480d8
Alí Sinisterra <haidarconh@gmail.com>
2025-10-26 07:06:06 -0500

    add 'website/new' endpoint to API

--- backstage/main.py
+++ backstage/main.py
@@ -9,4 +9,8 @@ v1Router = APIRouter(prefix="/v1", tags=["v1"])
 def getAllWebsites():
     return data.getWebsites()
 
+@v1Router.post('website/new')
+def createWebsite(website: data.Website):
+    data.addWebsite(website)
+
 app.include_router(v1Router)
