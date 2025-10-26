474febd11e9a77db7fd2b2e6967ae7677b3f0b6f
Alí Sinisterra <haidarconh@gmail.com>
2025-10-26 07:01:38 -0500

    rename v1_router to v1Router

--- backstage/main.py
+++ backstage/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi import APIRouter
 
 app = FastAPI()
-v1_router = APIRouter(prefix="/v1", tags=["v1"])
+v1Router = APIRouter(prefix="/v1", tags=["v1"])
 
 
 
-app.include_router(v1_router)
+app.include_router(v1Router)
