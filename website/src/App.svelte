59d6e2cafe52e47e681b80f49ca27c1d6094fab9
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 13:36:11 -0500

    import shared.svelte.ts into App.svelte

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import Map from "./lib/components/Map.svelte";
-  import RandomStringClicker from "./lib/components/RandomStringClicker.svelte";
+    import RandomStringClicker from "./lib/components/RandomStringClicker.svelte";
     import Header from "./lib/sections/Header.svelte"
     import Navigation from "./lib/sections/Navigation.svelte";
+    import './shared.svelte'
 </script>
 
 <main>
