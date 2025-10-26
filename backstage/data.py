09556bd6b53bcd787b98b0a7b8885da46fde5130
Alí Sinisterra <haidarconh@gmail.com>
2025-10-26 06:58:07 -0500

    create addWebsite() function on data.py, add Website BaseModel

--- backstage/data.py
+++ backstage/data.py
@@ -1,6 +1,8 @@
 # type: ignore      // pylance doesn't like dataset
 
 import dataset
+from pydantic import BaseModel
+
 DATABASE_URL = "sqlite:///_data.db"
 db = dataset.connect
 
@@ -9,3 +11,11 @@ websites = db['websites']
 def getWebsites():
     return websites.all()
 
+class Website(BaseModel):
+    id: str
+    name: str
+    url: str
+
+def addWebsite(website: Website):
+    with dataset.connect() as tx:
+        tx['websites'].insert(website)
