ebf3c62c080e8581ca9bc5b22464e30436cfca44
Alí Sinisterra <haidarconh@gmail.com>
2025-10-26 07:04:36 -0500

    add 'websites/all' endpoint to API

--- backstage/main.py
+++ backstage/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
+import data
 
 app = FastAPI()
 v1Router = APIRouter(prefix="/v1", tags=["v1"])
 
-
+@v1Router.get('websites/all')
+def getAllWebsites():
+    return data.getWebsites()
 
 app.include_router(v1Router)
