769c0f2f7741065380919bb16800146555ccd06d
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 13:09:41 -0500

    export ky api from api.ts

--- website/src/api.ts
+++ website/src/api.ts
@@ -1,5 +1,15 @@
 import * as config from '../config' 
+import ky from 'ky'
 
-export const getAllWebsites = () => {
-    
-}
+export const api = ky.create({
+  prefixUrl: config.API_URL,
+  timeout: 10000,
+  hooks: {
+    beforeError: [
+      (error) => {
+        console.error("API Error:", error);
+        return error;
+      },
+    ],
+  },
+});
