dde131f32d37a105fefe9f75b0e50f05dedc5d12
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 19:34:30 -0500

    copy Button element from old project

---
+++ website/src/lib/components/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { content = $bindable("click me!"), action, disabled = $bindable(false), id = "button-r", primary = false } = $props()
+</script>
+
+<button onclick={action} {disabled} {id} class={primary ? "primary" : "secondary"}>
+    {content}
+</button>
