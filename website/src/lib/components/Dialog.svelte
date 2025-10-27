d79b1dfa94296d2cb52f2c35f5a3c3692f4049a1
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 19:35:12 -0500

    start work on adapting Dialog element from other project

---
+++ website/src/lib/components/Dialog.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+
+    let { content, title = "Dialog", shown = $bindable() } = $props()
+    let dialogReference: HTMLDialogElement
+    
+    $effect(() => {
+        if (shown) {
+            dialogReference.showModal()
+        } else {
+            dialogReference.close()
+        }
+    })
+</script>
+
+<dialog bind:this={dialogReference} onclose={() => shown = false} closedby="any">
+    <nav>
+        <h2>{title}</h2>
+        <Button id="close-button" content="Close" action={() => { shown = false }} />
+    </nav>
+    {@render content()}
+</dialog>
+
+<style>
+    dialog {
+        width: clamp(280px, 100%, 560px);
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: flex-start;
+
+        color: inherit;
+        font-size: inherit;
+        border: var(--i-border-primary);
+        border-radius: var(--i-radius-primary);
+        background-color: var(--i-color-overlay-background);
+
+        animation: 100ms dialogEnter;
+        animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+        transition-behavior: allow-discrete;
+        transition: all 50ms;
+
+        align-self: center;
+        justify-self: center;
+    }
+
+    dialog:focus {
+        outline: var(--t-border-secondary);
+        outline-offset: 3pt;
+    }
+
+    @keyframes dialogEnter {
+        0% {
+            transform: scale(0.98);
+            opacity: 0.7;
+        }
+        100% {
+            transform: none;
+            opacity: 1;
+        }
+    }
+
+    dialog::backdrop {
+        animation: 600ms backdropEnter forwards;
+        background-color: #fff;
+        opacity: 0.5;
+    }
+
+    dialog nav {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    :global(dialog h2, dialog h3, dialog p) {
+        font-size: 1.5rem;
+    }
+
+    :global(#close-button) {
+        align-self: flex-end;
+    }
+
+    :global(ol) {
+        max-width: 100%;
+        list-style-type: decimal;
+    }
+
+    dialog:not([open]) {
+        display: none;
+        transition: all;
+    }
+</style>
