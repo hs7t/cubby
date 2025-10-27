307b36b1a653328ecabeac0b7530fd8fdeea674d
Alí Sinisterra <haidarconh@gmail.com>
2025-10-27 17:38:37 -0500

    style Button

--- website/src/lib/components/Button.svelte
+++ website/src/lib/components/Button.svelte
@@ -5,3 +5,30 @@
 <button onclick={action} {disabled} {id} class={primary ? "primary" : "secondary"}>
     {content}
 </button>
+
+<style>
+    button {
+        font-size: 1.2rem;
+        padding: 0.4ch;
+        font-weight: 600;
+        text-transform: uppercase;
+        background-color: var(--c-color-background-B);
+        color: var(--c-color-primary);
+        border: var(--c-border-generic);
+        font-family: var(--c-font-family-primary);
+        transition: all 100ms;
+    }
+
+    button:focus {
+        outline: var(--c-border-attention);
+        outline-offset: 3pt;
+    }
+
+    button:active {
+        transform: scale(1.08);
+    }
+
+    button:hover {
+        opacity: 0.9;
+    }
+</style>
