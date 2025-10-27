61f01c5099dc78befd5b7d022e24e44c4db7d123
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 20:02:15 -0500

    add shown prop to WebsiteInfoOverlay

--- website/src/lib/components/WebsiteInfoOverlay.svelte
+++ website/src/lib/components/WebsiteInfoOverlay.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import type { Website } from "../../shared.svelte";
-    let { website = undefined as Website|undefined } = $props()
+    let { website = undefined as Website|undefined, shown = $bindable(false) } = $props()
     import Dialog from "./Dialog.svelte";
 </script>
 
-<Dialog>
+<Dialog bind:shown={shown}>
     {#snippet content()}
         <h2>{website?.name}</h2>
     {/snippet}
