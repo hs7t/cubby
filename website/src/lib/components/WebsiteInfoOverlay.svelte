3b748eae89dccc7e006bde88cea2bbd1bfd9b414
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-26 19:53:47 -0500

    create WebsiteInfoOverlay component

---
+++ website/src/lib/components/WebsiteInfoOverlay.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { Website } from "../../shared.svelte";
+    let { website = undefined as Website|undefined } = $props()
+    import Dialog from "./Dialog.svelte";
+</script>
+
+<Dialog>
+    {#snippet content()}
+        <h2>{website?.name}</h2>
+    {/snippet}
+</Dialog>
