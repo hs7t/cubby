d9dde424be844e524f87d9da921a04f034209e53
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-27 14:10:06 -0500

    add animation to Header heading

--- website/src/lib/sections/Header.svelte
+++ website/src/lib/sections/Header.svelte
@@ -1,5 +1,58 @@
+<script lang="ts">
+    import confetti from 'canvas-confetti'
+    var scalar = 2;
+    var leaf = confetti.shapeFromText({ text: '🍂', scalar });
+
+    var count = 200;
+
+    var defaults = {
+        spread: 360,
+        ticks: 60,
+        gravity: 0,
+        decay: 0.96,
+        startVelocity: 20,
+        shapes: [leaf],
+        scalar,
+        origin: { y: 2 }
+    };
+
+    function fire(particleRatio: number, opts: object) {
+        confetti({
+            ...defaults,
+            ...opts,
+            particleCount: Math.floor(count * particleRatio)
+        });
+    }
+
+    const handleHeadingClick = () => {
+    fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+    });
+    fire(0.2, {
+    spread: 60,
+    });
+    fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+    });
+    fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+    });
+    fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+    });    }
+</script>
+
 <header>
-    <h1>cubby.website</h1>
+    <a onclick={handleHeadingClick}>
+        <h1>cubby.website</h1>
+    </a>
 </header>
 
 <style>
