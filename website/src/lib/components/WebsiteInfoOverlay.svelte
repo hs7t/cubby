89e1b8b7eb301a9c55a74189732cb59e8842ec60
Alí Sinisterra <haidarconh@gmail.com>
2025-10-27 19:20:17 -0500

    add NavigateButton to WebsiteInfoOverlay

--- website/src/lib/components/WebsiteInfoOverlay.svelte
+++ website/src/lib/components/WebsiteInfoOverlay.svelte
@@ -2,6 +2,7 @@
     import type { Website } from "../../shared.svelte";
     let { website = undefined as Website|undefined, shown = $bindable(false) } = $props()
     import Dialog from "./Dialog.svelte";
+    import NavigateButton from "./NavigateButton.svelte";
 </script>
 
 <Dialog bind:shown={shown} title={website?.name}>
@@ -18,5 +19,6 @@
                 {/each}
             </ol>
         </section>
+        <NavigateButton url={website?.url} />
     {/snippet}
 </Dialog>
