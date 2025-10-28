c1f38ecbcc3e83d4a298abdca9b038c45834847f
Alí Sinisterra <haidarconh@gmail.com>
2025-10-27 19:19:21 -0500

    create NavigateButton

---
+++ website/src/lib/components/NavigateButton.svelte
@@ -0,0 +1,35 @@
+<script>
+    let { content = "Navigate", url, disabled = $bindable(false), id = "button-navigate" } = $props()
+</script>
+
+<button onclick={() => window.open(url, "_blank", 'location=yes,height=570,width=520,scrollbars=yes,status=yes')} {disabled} {id}>
+    {content}
+</button>
+
+<style>
+    button {
+        font-size: 1.5rem;
+        padding: 0.5ch;
+        font-weight: 600;
+        text-transform: uppercase;
+        background-color: var(--c-color-background-B);
+        color: var(--c-color-primary);
+        border: var(--c-border-generic);
+        font-family: var(--c-font-family-primary);
+        transition: all 100ms;
+    }
+
+    button:focus {
+        outline: var(--c-border-attention);
+        outline-offset: 3pt;
+    }
+
+    button:active {
+        transform: translateY(-0.3rem);
+    }
+
+    button:hover {
+        transform: scale(1.08);
+        opacity: 0.9;
+    }
+</style>
