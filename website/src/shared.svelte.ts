120f384c6d06fbbb414380d479fef51a1b85c1e1
Alí Sinisterra <haidarconh@gmail.com>
2025-12-28 11:06:24 -0500

    🔥 remove api.ts and references

--- website/src/shared.svelte.ts
+++ website/src/shared.svelte.ts
@@ -1,32 +1,28 @@
-import { api, parseResponseJSON } from "./api";
-
 export type Marker = {
-  coordinates: Array<number>;
-  title: string;
-  action: Function;
-  id: string;
-};
-
-export type Website = {
-  cubbyId: string;
-  name: string;
-  url: string;
-  address: string;
-  mapCoordinates: Array<number>;
-  review: string;
-  directions: Array<string>;
-};
-
-const shuffle = (array: Array<any>) => {
-  return array.sort(() => Math.random() - 0.5);
+    coordinates: Array<number>
+    title: string
+    action: Function
+    id: string
 }
 
-export const websites = await shuffle((await parseResponseJSON(
-  await api.get("websites/all")
-))) as Array<Website>;
+export type Website = {
+    cubbyId: string
+    name: string
+    url: string
+    address: string
+    mapCoordinates: Array<number>
+    review: string
+    directions: Array<string>
+}
+
+const shuffle = (array: Array<any>) => {
+    return array.sort(() => Math.random() - 0.5)
+}
+
+export const websites = undefined as Array<Website>
 console.log(websites)
 
 export const uiState = $state({
-  selectedWebsite: undefined as Website|undefined,
-  websiteInfoOverlayShown: false
+    selectedWebsite: undefined as Website | undefined,
+    websiteInfoOverlayShown: false,
 })
